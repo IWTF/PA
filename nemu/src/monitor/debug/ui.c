693838f19b9720628586f1ed9ae4770ea87d3f58
tracer-ics2017 <tracer@njuics.org>
2019-03-30 10:32:38 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  10:32:38 up  3:12,  1 user,  load average: 0.20, 0.41, 0.53 24b01fc1b5b27f00400b2f6e936906cc8a9dafa1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -129,9 +129,9 @@ static int cmd_x(char *args) {
 
   // 循环使用 vaddr_read 函数来读取内存
   for(int i=0; i<count; i++) {
-      uint32_t a = vaddr_read(addr, 1);    //如何调用，怎么传递参数，请阅读代码
+      uint32_t a = vaddr_read(addr, i);    //如何调用，怎么传递参数，请阅读代码
       //每次循环将读取到的数据用 printf 打印出来
-      printf("get data is %u", a);    //如果你不知道应该打印什么，可以参考参考输出形式
+      printf("get data is %.2x\n", a);    //如果你不知道应该打印什么，可以参考参考输出形式
   }
   return 0;
 } // my function end
