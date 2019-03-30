b29ff5ad77d6b227b84a6df076f50f340e998c4d
tracer-ics2017 <tracer@njuics.org>
2019-03-30 09:51:11 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  09:51:11 up  2:30,  1 user,  load average: 0.56, 0.57, 0.46 f7417887a3ff3530e79345863dcc9431b22b1dc2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -80,7 +80,7 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args) {
   //分割字符串，得到起始位置和要读取的次数
-  uint32_t addr = atoi(strtok(NULL, " "));
+  int addr = atoi(strtok(NULL, " "));
   int count = atoi(strtok(NULL, " "));
   printf("addr is %u\n", addr);
   printf("count is %d\n", count);
