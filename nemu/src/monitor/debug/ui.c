a51b09c2806cbe464acf6b1e07fe1009ad3af2a9
tracer-ics2017 <tracer@njuics.org>
2019-04-05 16:25:13 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  16:25:13 up  6:43,  1 user,  load average: 0.23, 0.34, 0.42 2dae7be7e7427b31becb135d46b62ba1bccc959

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -128,7 +128,7 @@ static int cmd_x(char *args) {
 } 
 
 static int cmd_p(char *args) {
-  char *arg = strtok(NULL, " ");
+  char *arg = strtok(NULL, "");
   printf("p 参数：%s\n", arg);
 
   return 0;
