54e8513f99cbd2bec9cd3740e80cbc3891309199
tracer-ics2017 <tracer@njuics.org>
2019-04-15 13:32:59 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  13:32:59 up  3:37,  1 user,  load average: 0.53, 0.37, 0.30 b38b4160d035cd4d91b206c19b1004e26c987254

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -28,7 +28,7 @@ WP *new_wp(char *e) {
 
 	// 获取该watchpoint的表达式
 	// strcpy(free_->expr, e);
-	printf("get expr is %s\n", e);
+	printf("watchpoint no is%d\n", free_->NO);
 
 	// 获取该watchpoint的值
 	bool success = true;
