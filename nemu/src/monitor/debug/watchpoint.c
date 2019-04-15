518701bb428cf54857232b6b971ec80420c8e452
tracer-ics2017 <tracer@njuics.org>
2019-04-15 21:15:16 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  21:15:16 up  1:51,  1 user,  load average: 0.29, 0.32, 0.29 bb81bd7dbf4aaf4bf598588bf8de24c366e3693

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -91,11 +91,12 @@ WP *scan_watchpoint() {
     while (p) {
       bool success = true;
       uint32_t value = expr(p->expr, &success);
-      printf("%s is %d\n", p->expr, value);
+      // printf("%s is %d\n", p->expr, value);
       p->new_val = value;
       if (p->new_val != p->old_val) {
         return p;
       }
+      p = p->next;
     }
     return NULL;
 }
