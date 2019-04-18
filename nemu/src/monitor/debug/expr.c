dcffd3eabbfa5da80b60d2978dbca858cd7f7e1c
tracer-ics2017 <tracer@njuics.org>
2019-04-18 22:07:08 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  22:07:08 up  1:17,  1 user,  load average: 0.42, 0.35, 0.31 f42fe20aae20b1af33093e7ecde3038ec4535f21

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -185,10 +185,12 @@ uint32_t find_dominated_op(int p, int q) {
       if (tokens[i].type == TK_AND) cur_p = 6;
       if (tokens[i].type == TK_OR) cur_p = 5;
 
-      printf("NO.%d cur_p is:%d\n", i, cur_p);
-      if (cur_p < pre_p) {
-        temp = i;
-        pre_p = cur_p;
+      if (tokens[i].type != TK_HEX && tokens[i].type != TK_OCT && TK_REG) {
+        printf("NO.%d cur_p is:%d\n", i, cur_p);
+        if (cur_p < pre_p) {
+          temp = i;
+          pre_p = cur_p;
+        }
       }
     }
     if (flag < 0) {
