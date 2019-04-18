4539cd60efbc80aaf582f0a194995ed15811b3fd
tracer-ics2017 <tracer@njuics.org>
2019-04-18 23:27:34 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  23:27:34 up 33 min,  1 user,  load average: 0.24, 0.33, 0.34 1362787ceaea7cc14d5edc088a9038f43312eb6

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -42,9 +42,9 @@ void cpu_exec(uint64_t n) {
     //       printf("  new value = %d\n", p->new_val);
     //       p->old_val = p->new_val;
     //     } else {
-    //       char *addr = strtok(p->expr, " ");
-    //       addr = strtok(NULL, " ");
-    //       addr = strtok(NULL, " ");
+          // char *addr = strtok(p->expr, " ");
+          // addr = strtok(NULL, " ");
+          // addr = strtok(NULL, " ");
     //       printf("Breakpoint %d at %s\n", p->NO, addr);
     //       p->old_val = 0;
     //       p->new_val = 0;
