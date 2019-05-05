208fc97c37dcb2b5b1a176896701aa3d27112b1f
tracer-ics2017 <tracer@njuics.org>
2019-05-05 22:47:21 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  22:47:21 up 15 min,  1 user,  load average: 0.25, 0.30, 0.26 807512f3573a31af68cc78b75365931fd258cc1c

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -7,8 +7,9 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  printf("%#x\n", id_dest->val);
+
   rtl_and(&t0,&id_dest->val,&id_src->val);
+  printf("%#x\n", id_dest->val);
   operand_write(id_dest,&t0);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
   rtl_set_OF(0);
