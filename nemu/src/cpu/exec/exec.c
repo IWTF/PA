f2e09ca05c414f5c2f374faa1593b8331fbbd718
tracer-ics2017 <tracer@njuics.org>
2019-05-09 13:28:05 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  13:28:05 up 52 min,  1 user,  load average: 0.44, 0.44, 0.38 785f061d475649e4cc4e5fb75cd9248d54b304e

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -42,7 +42,7 @@ static make_EHelper(name) { \
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EX(and), EMPTY, EXW(sub, 1), EMPTY)
+    EX(and), EXW(sub, 1), EMPTY, EMPTY)
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
@@ -62,7 +62,7 @@ make_group(gp4,
   /* 0xff */
 make_group(gp5,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, IDEX(E, push), EMPTY, EMPTY)
+    EMPTY, EMPTY, IDEX(E, push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
