931c6379defd04a1d09a3dfb2594f6ca063ce576
tracer-ics2017 <tracer@njuics.org>
2019-05-11 07:22:52 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  07:22:52 up 26 min,  1 user,  load average: 0.17, 0.27, 0.23 73966efaa592371e11c64ab68a31fb11319cf66b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -238,6 +238,7 @@ void exec_wrapper(bool print_flag) {
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
   strcat(decoding.asm_buf, decoding.assembly);
   Log_write("%s\n", decoding.asm_buf);
+  printf("decoding.asm_buf: %s\n", decoding.asm_buf);
   if (print_flag) {
     puts(decoding.asm_buf);
   }
