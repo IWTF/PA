05f6ed96c3597a9d0b05ee4acd8abb602377ac0f
tracer-ics2017 <tracer@njuics.org>
2019-05-18 22:12:16 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  22:12:16 up  4:42,  1 user,  load average: 0.19, 0.28, 0.43 9da285a028052f17fd0aa808a03e2048e6b1982

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -105,6 +105,20 @@ make_EHelper(shr) {
   print_asm_template2(shr);
 }
 
+make_EHelper(rol) {
+  t0 = (id_dest->val>>(id_dest->width * 8 - id_src->val));
+  t1 = (id_dest->val<<id_src->val);
+  t1 = t1 + t0;
+  operand_write(id_dest, &t1);
+
+  rtl_update_ZFSF(&t1, id_dest->width);
+
+  t2 = t0 & 0x1;
+  rtl_set_CF(&t2);
+
+  print_asm_template2(rol);
+}
+
 make_EHelper(setcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
