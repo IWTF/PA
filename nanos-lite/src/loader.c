40869c58c210cfd48849bb38d4ac2e6b0c81a52b
tracer-ics2017 <tracer@njuics.org>
2019-06-07 13:16:59 +0800

    >  compile 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  13:16:59 up  2:44,  1 user,  load average: 0.48, 0.37, 0.22 77196f6d993bd09e560235d4b76326274ffdf

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,12 @@
 #include "common.h"
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+extern void ramdisk_read(void *buf, off_t offset, size_t len) ;
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, ((&ramdisk_end) - (&ramdisk_start)));
   return (uintptr_t)DEFAULT_ENTRY;
 }
--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -10,12 +10,14 @@ extern uint8_t ramdisk_end;
  */
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
 void ramdisk_read(void *buf, off_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
+// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
 void ramdisk_write(const void *buf, off_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(&ramdisk_start + offset, buf, len);
@@ -26,6 +28,7 @@ void init_ramdisk() {
       &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
+// 返回ramdisk的大小, 单位为字节
 size_t get_ramdisk_size() {
   return RAMDISK_SIZE;
 }
