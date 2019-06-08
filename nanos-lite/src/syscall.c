a4fc12861d0a899c717d7377b34fc47e03ed6c32
tracer-ics2017 <tracer@njuics.org>
2019-06-08 09:40:40 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  09:40:40 up  1:50,  1 user,  load average: 0.52, 0.37, 0.36 718fcf11350e303e9a41a6447784ee3e16d619a1

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,21 +2,30 @@
 #include "syscall.h"
 #include "fs.h"
 
+extern char _end;
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern int fs_close(int fd);
+
 int mm_brk(uint32_t new_brk);
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  TODO();
-  // 我们约定，这个系统调用什么都不用做，直接返回 1
+  // TODO();
+  return fs_open((char *)pathname, flags, mode);
   return 1;
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
+  // TODO();
+  return fs_write(fd, (void *)buf, len);
   return 1;
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
+  // TODO();
+  return fs_read(fd, (void *)buf, len);
   return 1;
 }
 
@@ -25,13 +34,14 @@ static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t when
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-  TODO();
+  // TODO();
+  return fs_close(fd);
   return 1;
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
-  TODO();
-  return 1;
+  // TODO();
+  return 0;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -45,14 +55,14 @@ _RegSet* do_syscall(_RegSet *r) {
   
   uintptr_t ret = -1;
   switch (a[0]) {
-    case SYS_none: ret = 1; break;
-    case SYS_exit: _halt(0); break;
-    case SYS_write: ret = fs_write(a[1], (void *)a[2], a[3]); break;
-    case SYS_brk: ret = 0; break;
-    case SYS_open: ret = fs_open((char *)a[1], a[2], a[3]); break;
-    case SYS_read: ret = fs_read(a[1], (void *)a[2], a[3]); break;
-    case SYS_lseek: ret = fs_lseek(a[1], a[2], a[3]); break;
-    case SYS_close: ret = fs_close(a[1]); break;
+    case SYS_none:  ret = 1; break;
+    case SYS_exit:  _halt(0); break;
+    case SYS_write: ret = sys_write(a[1], a[2], a[3]); break;
+    case SYS_brk:   ret = 0; break;
+    case SYS_open:  ret = sys_open(a[1], a[2], a[3]); break;
+    case SYS_read:  ret = sys_read(a[1], a[2], a[3]); break;
+    case SYS_lseek: ret = sys_lseek(a[1], a[2], a[3]); break;
+    case SYS_close: ret = sys_close(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   SYSCALL_ARG1(r) = ret;
