103f2f76a45b021758c8f2535fffce50a529faf3
tracer-ics2017 <tracer@njuics.org>
2019-06-08 08:00:14 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  08:00:14 up 9 min,  1 user,  load average: 0.86, 0.57, 0.35 a8323a6c82cb9b4139d30d4cf4407bd254136c8a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,10 +37,14 @@ static inline uintptr_t sys_brk(uintptr_t new_brk) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
   
   uintptr_t ret = -1;
   switch (a[0]) {
     case SYS_none: ret = 1; break;
+    case SYS_exit: _halt(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   SYSCALL_ARG1(r) = ret;
