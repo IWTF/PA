564e6744990ba8865e4fb4c107f0e98e30b77d2b
tracer-ics2017 <tracer@njuics.org>
2019-06-08 10:03:29 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  10:03:29 up  2:13,  1 user,  load average: 0.31, 0.27, 0.28 9c4865066caf58317653a6298ec742bd1acd30e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,10 +3,16 @@
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 extern void ramdisk_read(void *buf, off_t offset, size_t len) ;
+extern int fs_close(int fd);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t count);
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY, 0, ((&ramdisk_end) - (&ramdisk_start)));
+  // ramdisk_read(DEFAULT_ENTRY, 0, ((&ramdisk_end) - (&ramdisk_start)));
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, DEFAULT_ENTRY, ((&ramdisk_end) - (&ramdisk_start)));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
