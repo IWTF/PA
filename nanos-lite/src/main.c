0794864bef3a35dc9d8c5bcadd687fe097c177fe
tracer-ics2017 <tracer@njuics.org>
2019-06-21 19:49:13 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:49:13 up  1:33,  1 user,  load average: 0.84, 0.58, 0.45 13b95e949284cc1819dfa3a3da76735ffef1b26

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,8 +34,10 @@ int main() {
   // uint32_t entry = loader(NULL, "/bin/pal");
   // ((void (*)(void))entry)();
   // load_prog("/bin/dummy");
-  load_prog("/bin/pal");
+  // load_prog("/bin/pal");
+  
   load_prog("/bin/hello");
+  // load_prog("/bin/hello");
 
   _trap();
 
