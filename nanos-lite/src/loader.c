67480e0189bc411b3b9a77f248ec1aad3924a987
tracer-ics2017 <tracer@njuics.org>
2019-06-21 18:40:27 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  18:40:27 up 25 min,  1 user,  load average: 0.51, 0.74, 0.71 1cc98e7bf4b48a7af643c7a8b4bfa2f799c7b567

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,7 +45,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
   if (file_size > 0) {
   	pa = new_page();
 
-  	Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+  	// Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
 	_map(as, va, pa);
 	fs_read(fd, pa, file_size);
   }
