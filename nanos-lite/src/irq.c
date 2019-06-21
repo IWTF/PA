d734758e2f7a807b4a842ba60d238186ecd82857
tracer-ics2017 <tracer@njuics.org>
2019-06-21 19:29:56 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:29:56 up  1:14,  1 user,  load average: 0.62, 0.44, 0.38 46cfc07d2fc797d286065d75948326e0d635fffe

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,9 +5,9 @@ extern _RegSet* schedule(_RegSet *prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   _RegSet *ret = NULL;
-  Log("e.event is:%d\n", e.event);
-  Log("_EVENT_SYSCALL is:%d\n", _EVENT_SYSCALL);
-  Log("_EVENT_TRAP is:%d\n", _EVENT_IRQ_TIME);
+  // Log("e.event is:%d\n", e.event);
+  // Log("_EVENT_SYSCALL is:%d\n", _EVENT_SYSCALL);
+  // Log("_EVENT_TRAP is:%d\n", _EVENT_IRQ_TIME);
   switch (e.event) {
   	case _EVENT_SYSCALL: do_syscall(r); break;
   	case _EVENT_TRAP: ret = schedule(r); break;
