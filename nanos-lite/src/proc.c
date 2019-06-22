73942c57b0e8cf278ab0d884212a429b9ddf0f93
tracer-ics2017 <tracer@njuics.org>
2019-06-22 11:01:29 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  11:01:29 up 16:46,  1 user,  load average: 0.21, 0.39, 0.40 99a94fd23d7f5667550a532225b7d6852fdc0c4

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,18 +33,10 @@ void switch_game() {
 
 _RegSet* schedule(_RegSet *prev) {
   // return NULL;
-  // current->tf = prev;
-
-  // current = (current == current_game ? &pcb[1] : current_game);
-
-  // _switch(&current->as);
-  // save the context pointer
   current->tf = prev;
 
-  // always select pcb[0] as the new process
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = (current == current_game ? &pcb[1] : current_game);
 
-  // TODO: switch to the new address space,
-  // then return the new context
+  // _switch(&current->as);
   return current->tf;
 }
