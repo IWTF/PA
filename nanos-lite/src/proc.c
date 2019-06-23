0fef3a51eb47d3bfa205ec463a881c479bcddb50
tracer-ics2017 <tracer@njuics.org>
2019-06-23 12:37:49 +0800

    >  run 161720227 huangziwen Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  12:37:49 up  4:09,  1 user,  load average: 0.22, 0.21, 0.18 9c1d9eeb1792eb85275d7c55940fd1605ca1112c

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,13 +28,14 @@ void load_prog(const char *filename) {
 
 static PCB *current_game = &pcb[0];
 void switch_game() {
-  current_game = (current_game == &pcb[0] ? &pcb[2] : &pcb[0]);
+  current_game = (current_game == &pcb[0] ? &pcb[1] : &pcb[0]);
 }
 
 _RegSet* schedule(_RegSet *prev) {
-  return NULL;
+  // return NULL;
   current->tf = prev;
 
+  // current 指针指向当前运行进程的 PCB
   current = (current == current_game ? &pcb[1] : current_game);
 
   _switch(&current->as);
